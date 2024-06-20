a87b99c5b6d6011fddd227fc099361562c9f4c42
viviilunga012 <vvivianne26@gmail.com>
2024-06-20 12:48:24 +0200

    rechercher un itinéraire

---
+++ Recherche Itinerqires.c
@@ -0,0 +1,54 @@
+// Fonction pour rechercher des itinéraires de bus disponibles
+void rechercherItineraires() {
+    printf("Voici les itineraires disponibles :\n");
+
+    // Itinéraire 1 : lubumbashi - kinshasa
+    printf("1. lubumbashi - kinshasa\n");
+    printf("   Prix : 250,000 FC | 150 USD\n");
+    printf("----------------------------------------------------\n");
+
+    // Itinéraire 2 : lubumbashi - likasi
+    printf("2. lubumbashi - likasi\n");
+    printf("   Prix : 100,000 FC | 60 USD\n");
+    printf("----------------------------------------------------\n");
+
+    // Itinéraire 3 : lubumbashi - kipushi
+    printf("3. lubumbashi - kipushi\n");
+    printf("   Prix : 80,000 FC | 50 USD\n");
+    printf("----------------------------------------------------\n");
+
+    // Itinéraire 4 : lubumbashi - kolwezi
+    printf("4. lubumbashi - kolwezi\n");
+    printf("   Prix : 200,000 FC | 120 USD\n");
+    printf("----------------------------------------------------\n");
+
+    // Itinéraire 5 : lubumbashi - kisangani
+    printf("5. lubumbashi - kisangani\n");
+    printf("   Prix : 350,000 FC | 210 USD\n");
+    printf("----------------------------------------------------\n");
+
+    // Itinéraire 6 : lubumbashi - goma
+    printf("6. lubumbashi - goma\n");
+    printf("   Prix : 300,000 FC | 180 USD\n");
+    printf("----------------------------------------------------\n");
+
+    // Itinéraire 7 : lubumbashi - kalemi
+    printf("7. lubumbashi - kalemi\n");
+    printf("   Prix : 180,000 FC | 110 USD\n");
+    printf("----------------------------------------------------\n");
+
+    // Itinéraire 8 : lubumbashi - bukavu
+    printf("8. lubumbashi - bukavu\n");
+    printf("   Prix : 240,000 FC | 145 USD\n");
+    printf("----------------------------------------------------\n");
+
+    // Itinéraire 9 : lubumbashi - lusaka
+    printf("9. lubumbashi - lusaka\n");
+    printf("   Prix : 200,000 FC | 120 USD\n");
+    printf("----------------------------------------------------\n");
+
+    // Itinéraire 10 : lubumbashi - nkongolo
+    printf("10. lubumbashi - nkongolo\n");
+    printf("    Prix : 120,000 FC | 75 USD\n");
+    printf("----------------------------------------------------\n");
+}
